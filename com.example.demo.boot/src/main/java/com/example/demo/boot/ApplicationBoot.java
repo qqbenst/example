c6a22732ab6f48ed7d4eb4ca1fe94f7f03955f41
ben <631910564@qq.com>
2017-06-04 17:22:31 +0800

    修改pom打包方式

--- com.example.demo.boot/src/main/java/com/example/demo/boot/ApplicationBoot.java
+++ com.example.demo.boot/src/main/java/com/example/demo/boot/ApplicationBoot.java
@@ -2,8 +2,10 @@ package com.example.demo.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
+@ComponentScan(basePackages = {"com.example.demo"})
 public class ApplicationBoot {
 	 public static void main(String[] args) throws Exception {
 	        SpringApplication.run(ApplicationBoot.class, args);
--- com.example.demo.web/src/main/java/com/example/demo/web/view/TestController.java
+++ com.example.demo.web/src/main/java/com/example/demo/web/view/TestController.java
@@ -1,12 +1,14 @@
 package com.example.demo.web.view;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/test/")
 public class TestController {
 
+	@RequestMapping(value = "hello/", method = RequestMethod.GET)
 	public void saveHello(){
 		System.out.println("hello");
 	}
