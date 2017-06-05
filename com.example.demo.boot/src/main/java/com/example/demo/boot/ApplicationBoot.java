537a5181c888664f17a9d9d243ff2d36f58d2bd5
ben <631910564@qq.com>
2017-06-05 20:36:18 +0800

    test

--- com.example.demo.boot/src/main/java/com/example/demo/boot/ApplicationBoot.java
+++ com.example.demo.boot/src/main/java/com/example/demo/boot/ApplicationBoot.java
@@ -1,7 +1,5 @@
 package com.example.demo.boot;
-/**
- * dsfsdfsdfsdfsd
- */
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
@@ -9,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 @ComponentScan(basePackages = {"com.example.demo"})
 public class ApplicationBoot {
-	//
+	
 	 public static void main(String[] args) throws Exception {
 	        SpringApplication.run(ApplicationBoot.class, args);
 	    }
